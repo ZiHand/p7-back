b7f416ea46cd8a3a40174059e769b401b0a19d46
Ziggy <ziggyhand.dev@gmail.com>
2022-05-24 17:22:47 +0200

    Generate uuid for pic post

--- js/models/post_model.js
+++ js/models/post_model.js
@@ -1,38 +1,33 @@
-const Sequelize     = require('sequelize');
-const db            = require('../../config/db');
-const CommentModel  = require("./comment_model");
-const UserModel     = require("./user_model");
+const Sequelize = require("sequelize");
+const db = require("../../config/db");
+const CommentModel = require("./comment_model");
+const UserModel = require("./user_model");
 
 // ===================================================
 //                 Post Model
 // ===================================================
-const postSchema = db.define('posts', 
-{
-    id : 
-    {
-        type            : Sequelize.UUID,
-        defaultValue    : Sequelize.UUIDV4,
-        primaryKey      : true,
-        allowNull       : false
-    },
-    message : 
-    {
-        type            : Sequelize.DataTypes.STRING(500),
-        empty           : true
-    },
-    picture : 
-    {
-        type            : Sequelize.DataTypes.STRING(150),
-        empty           : true
-    },
-    video : 
-    {
-        type            : Sequelize.DataTypes.STRING(200),
-        empty           : true
-    }
+const postSchema = db.define("posts", {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  message: {
+    type: Sequelize.DataTypes.STRING(500),
+    empty: true,
+  },
+  picture: {
+    type: Sequelize.DataTypes.STRING(150),
+    empty: true,
+  },
+  video: {
+    type: Sequelize.DataTypes.STRING(200),
+    empty: true,
+  },
 });
 
 // ===================================================
 //                 Post Export
 // ===================================================
-module.exports = postSchema ;
+module.exports = postSchema;
