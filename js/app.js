484fc66d76f79006cb29db62a0bde117678faa5d
Ziggy <ziggyhand.dev@gmail.com>
2022-05-26 20:47:54 +0200

    Add cool face API

--- js/app.js
+++ js/app.js
@@ -36,7 +36,10 @@ app.use(cookieParser());
 app.use(cors(corsOptions));*/
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://p7-front.zihand.fr");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://p7-front.zihand.fr/login"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
