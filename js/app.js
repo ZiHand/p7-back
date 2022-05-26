28d0ad36e836f4e80cf1dfa40f75ec1a80e3a9b5
Ziggy <ziggyhand.dev@gmail.com>
2022-05-26 19:33:54 +0200

    Add cool face API

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://p7-front.zihand.fr");
   res.header(
     "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Credentials",
     "Origin, X-Requested, Content-Type, Accept Authorization"
   );
   if (req.method === "OPTIONS") {
