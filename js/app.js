cdce7e71914463c6cb4d98c9e67453e045170a36
Ziggy <ziggyhand.dev@gmail.com>
2022-05-26 17:45:45 +0200

    Add cool face API

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // ===================================================
 const corsOptions = {
   origin: "*",
-  credentials: true,
+  credentials: false,
   allowedHeaders: ["sessionId", "Content-Type"],
   exposedHeaders: ["sessionId"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
