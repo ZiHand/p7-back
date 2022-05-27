3b94c76b3c0322d9497cf6e88bab6c11013fb5bf
Ziggy <ziggyhand.dev@gmail.com>
2022-05-27 17:29:12 +0200

    Add cool face API

--- js/controllers/auth_controller.js
+++ js/controllers/auth_controller.js
@@ -50,7 +50,6 @@ module.exports.login = async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       maxAge: maxAge,
-      sameSite: "none",
     });
     res.status(200).json({ user: user.id });
     console.log("login token", token);
